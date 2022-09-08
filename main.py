ad9fc3fb2b7be76bde963f2c26dda1aa8aaa8073
HarrisAffandi708 <harris.ilhan.bin@gmail.com>
2022-09-08 13:24:18 +0100

    added test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = discord.Bot(command_prefix='$',intents=intents)
+bot = commands.Bot(command_prefix='$',intents=intents)
 
 @bot.event
 async def on_ready():
@@ -22,6 +22,11 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
 
 bot.run('MTAxNzM1MTQyNzU4MjA3NDkzMQ.GQVoQF.Js6DsOEzbLZeVpda5he-ahqEU53pZKvV3dOUU4')
 
