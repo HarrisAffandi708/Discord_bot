320c91e291bb43a62f78f92dfb4c013102da6867
HarrisAffandi708 <harris.ilhan.bin@gmail.com>
2022-09-08 15:08:37 +0100

    added new functions

--- main.py
+++ main.py
@@ -9,23 +9,34 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$',intents=intents)
+bot = commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+"""
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
-
-@bot.command()
-async def test(ctx, arg):
+"""
+@bot.command(pass_context=True)
+async def test(ctx, *, arg):
     await ctx.send(arg)
+
+@bot.command(pass_context=True)
+async def retard(ctx):
+    retard = "zi xuan is a retard"
+    await ctx.send(retard)
+
+@bot.command(pass_context=True)
+async def ship(ctx, a:str, b:str):
+    ship_statement = " should date "
+    await ctx.send(a+ship_statement+b)
 
 
 bot.run('MTAxNzM1MTQyNzU4MjA3NDkzMQ.GQVoQF.Js6DsOEzbLZeVpda5he-ahqEU53pZKvV3dOUU4')
